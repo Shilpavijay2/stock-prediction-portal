4724502713b631e83bf78ed41b67305ba89ea546
Shilpa Vijay <shilpavijay2@gmail.com>
2026-04-08 02:45:31 +0530

    homepage react

--- frontend-react/stock-prediction-portal/src/App.jsx
+++ frontend-react/stock-prediction-portal/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-
+import Header from './components/Header'
+import './App.css'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import './assets/css/style.css'
+import Button from './components/Button'
 
 function App() {
-  
-
   return (
     <>
-     
+      <Header />
+      <Main />
+      <Footer />
+      <Button />
     </>
   )
 }
